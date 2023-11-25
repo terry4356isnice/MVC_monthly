e784ad95d0e43a1c937db043e798cb65380561f4
terry4356isnice <terry4356nice@gmail.com>
2023-11-25 16:36:26 +0800

    the first edit

---
+++ MVC_monthly/Models/homepage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_monthly.Models
+{
+    public class homepage
+    {
+        public int ID { get; set; }
+        public string TITLE { get; set; }
+        public string IMAGE { get; set; }
+        public string LINK { get; set; }
+
+    }
+}
